d04a9396b625b89aa91334eaf1f61927ae4c8916
Richard Schulze <r.schulze@uni-muenster.de>
2026-03-13 13:52:49 +0100

    Update output message in main.sh to clarify total runs count

--- .template/scripts/main.sh
+++ .template/scripts/main.sh
@@ -161,7 +161,7 @@ main() {
     manifest_runs=$(awk -F'\t' '/^[0-9]+\t/ {c++} END{print c+0}' "$manifest_path")
     manifest_tasks=$(awk -F'\t' '/^[0-9]+\t/ {print $3}' "$manifest_path" | sort -u | wc -l)
     num_stages=$((max_stage_manifest + 1))
-    echo "Running $manifest_tasks task(s) with $manifest_runs run(s) in $num_stages stage(s)."
+    echo "Running $manifest_tasks task(s) with a total of $manifest_runs run(s) in $num_stages stage(s)."
     echo ""
 
     if [[ "$all_direct" == true ]]; then
